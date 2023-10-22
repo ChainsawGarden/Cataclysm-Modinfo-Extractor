7ee61b81635c42ee6ed2073fc796dd0ee3fcfebd
ChainsawGarden <chainsawgarden@proton.me>
2023-10-22 14:57:03 -0400

    add main file

---
+++ CMExtractor.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+
+def get_id():
+    """Extracts the Mod's ID from the specified, properly formatted mod folder."""
+    pass
+
+def cmextract():
+    """Extracts the mod id."""
+    pass
+
+# main
+if(__name__ == "__main__"):
+    cmextract()
